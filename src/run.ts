1a64ff9da3dadc07d1b1b0d6779675fec6588008
Paul Marbach <pmarbach@spotify.com>
2020-03-27 12:28:14 -0400

    chore: enable cors

--- src/run.ts
+++ src/run.ts
@@ -2,4 +2,7 @@ import startServer from './server';
 
 startServer({
   port: process.env.LAS_PORT ? Number(process.env.LAS_PORT) : undefined,
+  useCors: process.env.LAS_USE_CORS
+    ? Boolean(process.env.LAS_USE_CORS)
+    : undefined,
 });
