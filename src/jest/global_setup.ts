58f9f5c74bba496104b77cdbf85cf0886babf591
Cristian Greco <cristianrgreco@gmail.com>
2020-04-05 11:36:23 +0100

    Use testcontainers' default wait strategy which waits for port

--- src/jest/global_setup.ts
+++ src/jest/global_setup.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 import { Duration, TemporalUnit } from 'node-duration';
-import { GenericContainer, Wait, StartedTestContainer } from 'testcontainers';
+import { GenericContainer, StartedTestContainer } from 'testcontainers';
 import { Pool } from 'pg';
 
 import logger from '../logger';
@@ -40,17 +40,7 @@ export default async () => {
     .withEnv('POSTGRES_USER', 'postgres')
     .withEnv('POSTGRES_PASSWORD', 'postgres')
     .withEnv('POSTGRES_DB', 'postgres')
-    .withWaitStrategy(
-      Wait.forLogMessage('listening on IPv4 address "0.0.0.0", port 5432'),
-    )
     .start();
-
-  // The postgres docker container prints that it's ready for connections twice, so we
-  // have to listen for another log line. However, this log message comes before the
-  // container is actually ready, so we need to wait a little.
-  await new Promise(resolve => {
-    setTimeout(resolve, 3000);
-  });
 
   // We have to use global setup because it's the only way in Jest to run code once before suite.
   // Unfortuantely Jest prevents us from writing globals that are read by the sutite.
