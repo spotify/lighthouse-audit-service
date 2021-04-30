0f83cf636c14dd336dca4328c6a7efe059761521
Erik Larsson <erik.larsson@schibsted.com>
2021-04-30 08:07:15 +0200

    fix: serialize tests to avoid concurrency bug
    Signed-off-by: Erik Larsson <erik.larsson@schibsted.com>

--- jest.config.js
+++ jest.config.js
@@ -17,5 +17,6 @@ module.exports = {
   ...require('@spotify/web-scripts/config/jest.config.js'),
   globalSetup: '<rootDir>/jest/global_setup.ts',
   globalTeardown: '<rootDir>/jest/global_teardown.ts',
+  maxWorkers: 1,
   resetModules: true,
 };
